1c65e4775cc650d13b24aafcaa4070206f6dd76b
ecedevere <sheykina.mariia@gmail.com>
2026-08-29 11:13:29 +0300

    task 4 is done

--- 1/src/App.jsx
+++ 1/src/App.jsx
@@ -1,18 +1,46 @@
 import './App.css'
-import { useState } from 'react'
+import Row from './components/Row.jsx'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
+const apiUrl = 'http://localhost:3001'
+
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    axios.get(`${apiUrl}/tasks`)
+      .then(response => {
+        setTasks(response.data)
+      })
+      .catch(error => {
+        alert(error.response.data ? error.response.data.message : error)
+      })
+}, [])
+
   const addTask = (event) => {
     event.preventDefault()
-    const description = task.trim()
-    if (!description) return
-    setTasks(currentTasks => [...currentTasks,description])
-    setTask('')
-  }
+    const newTask = { description: task }
+    axios.post(`${apiUrl}/tasks`, { task: newTask })
+      .then(response => {
+        setTasks(currentTasks => [...currentTasks, response.data])
+        setTask('')
+      })
+      .catch(error => {
+        alert(error.response ? error.response.data.error.message : error)
+  })
+}
+
+
   const deleteTask = (deleted) => {
-    setTasks(currentTasks => currentTasks.filter(item => item !==deleted))
-  }
+    axios.delete(`${apiUrl}/tasks/${deleted}`)
+      .then(response => {
+        setTasks(currentTasks => currentTasks.filter(item => item.id !== deleted))
+      })
+      .catch(error => {
+        alert(error.response ? error.response.data.error.message : error)
+      })
+}
 
   return (
     <div id="container">
@@ -26,16 +54,9 @@ function App() {
       <ul>
         {
           tasks.map(item => (
-            <li key={item}>
-              {item}
-              <button
-                className='delete-button'
-                onClick={() => deleteTask(item)}>
-                  Delete
-                </button>
-              </li>
+            <Row task={item} key={item.id} onDelete={deleteTask} />
           ))
-        }
+      }
       </ul>
     </div>
   )
