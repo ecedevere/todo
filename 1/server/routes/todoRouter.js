43ab8f0b47d81adfafebc90aa899baa4dfcc2bbb
ecedevere <sheykina.mariia@gmail.com>
2026-08-30 13:35:39 +0300

    task 6 is done

--- 1/server/routes/todoRouter.js
+++ 1/server/routes/todoRouter.js
@@ -1,5 +1,6 @@
 import { pool } from '../helper/db.js'
 import { Router } from 'express'
+import { auth } from '../helper/auth.js'
 
 const router = Router()
 
@@ -12,7 +13,7 @@ router.get('/', (req, res, next) => {
   })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', auth, (req, res, next) => {
   const { task } = req.body
   if (!task || !task.description || !task.description.trim()) {
     const error = new Error('Task is required')
@@ -28,7 +29,7 @@ router.post('/', (req, res, next) => {
     })
 })
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', auth, (req, res, next) => {
   const { id } = req.params
   pool.query('delete from task WHERE id = $1', [id], (err, result) => {
     if (err) {
