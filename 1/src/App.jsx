1fdf7461abdaafe8a9b5c0ea3628074983b1b9bf
ecedevere <sheykina.mariia@gmail.com>
2026-09-01 16:01:10 +0300

    task 7 is done

--- 1/src/App.jsx
+++ 1/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Row from './components/Row.jsx'
 import { useState,useEffect } from 'react'
+import { useUser } from './context/useUser'
 import axios from 'axios'
 
 const apiUrl = 'http://localhost:3001'
@@ -8,6 +9,8 @@ const apiUrl = 'http://localhost:3001'
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
+  const { user } = useUser()
+
   useEffect(() => {
     axios.get(`${apiUrl}/tasks`)
       .then(response => {
@@ -20,8 +23,9 @@ function App() {
 
   const addTask = (event) => {
     event.preventDefault()
+    const headers = { headers: { Authorization: `Bearer ${user.token}` } }
     const newTask = { description: task }
-    axios.post(`${apiUrl}/tasks`, { task: newTask })
+    axios.post(`${apiUrl}/tasks`, { task: newTask }, headers)
       .then(response => {
         setTasks(currentTasks => [...currentTasks, response.data])
         setTask('')
@@ -33,7 +37,8 @@ function App() {
 
 
   const deleteTask = (deleted) => {
-    axios.delete(`${apiUrl}/tasks/${deleted}`)
+    const headers = { headers: { Authorization: `Bearer ${user.token}` } }
+    axios.delete(`${apiUrl}/tasks/${deleted}`, headers)
       .then(response => {
         setTasks(currentTasks => currentTasks.filter(item => item.id !== deleted))
       })
--- 1/src/main.jsx
+++ 1/src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Authentication, { AuthenticationMode } from './screens/Authentication'
+import ProtectedRoute from './components/ProtectedRoute'
+import UserProvider from './context/UserProvider.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import NotFound from './screens/NotFound'
+
+const router = createBrowserRouter([
+  {
+    path: '/signin',
+    element: <Authentication authenticationMode={AuthenticationMode.SignIn} />,
+    errorElement: <NotFound />
+  },
+  {
+    path: '/signup',
+    element: <Authentication authenticationMode={AuthenticationMode.SignUp} />,
+    errorElement: <NotFound />
+  },
+  {
+    element: <ProtectedRoute />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <App />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </StrictMode>,
 )
